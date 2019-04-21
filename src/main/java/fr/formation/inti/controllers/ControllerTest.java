ad030c85d156929b6f9cea72142b211241d956ec
IN-ST-013 <IN-ST-013@IN-ST-013>
2019-04-21 17:09:28 +0200

    The born of a legend project

---
+++ src/main/java/fr/formation/inti/controllers/ControllerTest.java
@@ -0,0 +1,64 @@
+package fr.formation.inti.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ControllerTest {
+	
+	@RequestMapping("/accueil")
+	public String accueil(Model model)
+	{	
+		return "accueil";
+	}
+	
+	@RequestMapping("/faq")
+	public String faq(Model model)
+	{	
+		return "faq";
+	}
+	
+	@RequestMapping("/forgottenpsd")
+	public String forgottenpsd(Model model)
+	{	
+		return "forgottenpsd";
+	}
+	
+	@RequestMapping("/map")
+	public String map(Model model)
+	{	
+		return "map";
+	}
+	
+	@RequestMapping("/mesalertes")
+	public String mesalertes(Model model)
+	{	
+		return "mesalertes";
+	}
+	
+	@RequestMapping("/modifiermoncompte")
+	public String modifiermoncompte(Model model)
+	{	
+		return "modifiermoncompte";
+	}
+	
+	@RequestMapping("/monprofil")
+	public String monprofil(Model model)
+	{	
+		return "monprofil";
+	}
+	
+	@RequestMapping("/quisommesnous")
+	public String quisommesnous(Model model)
+	{	
+		return "Qui_sommes_nous";
+	}
+	
+	@RequestMapping("/signup")
+	public String signup(Model model)
+	{	
+		return "signup";
+	}
+
+}
---
+++ src/main/java/fr/formation/inti/controllers/Test_Controller.java
@@ -0,0 +1,16 @@
+package fr.formation.inti.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Test_Controller {
+	
+	@RequestMapping("/test")
+	public String requestMapping(Model model)
+	{
+		return "map";
+	}
+
+}
