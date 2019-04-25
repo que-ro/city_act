213c0bd4d1debdb50253f86ccb11ebde9a890fee
Henry <henry.begue@live.fr>
2019-04-25 16:12:02 +0200

    footer controler legalhtml legal.css

--- src/main/java/fr/formation/inti/controllers/Modifiermoncompte_Controller.java
+++ src/main/java/fr/formation/inti/controllers/Modifiermoncompte_Controller.java
@@ -1,5 +1,13 @@
 package fr.formation.inti.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import fr.formation.inti.repositories.IUsersDao;
+
+@Controller
 public class Modifiermoncompte_Controller {
 
+	@Autowired
+	IUsersDao dao_usr;
 }
