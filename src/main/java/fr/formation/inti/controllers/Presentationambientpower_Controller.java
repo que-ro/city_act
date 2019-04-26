be0c48aec08947117b532d867d5e37c0b8488ebf
IN-ST-004 <IN-ST-004@IN-ST-004.home>
2019-04-26 15:29:28 +0200

    last changes

---
+++ src/main/java/fr/formation/inti/controllers/Presentationambientpower_Controller.java
@@ -0,0 +1,36 @@
+package fr.formation.inti.controllers;
+
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import fr.formation.inti.entities.AmbientPower;
+import fr.formation.inti.repositories.IAmbientPowerDao;
+
+@Controller
+public class Presentationambientpower_Controller {
+	
+@Autowired
+IAmbientPowerDao dao_ap;
+
+@RequestMapping(value="/presentationprojet_method", method = RequestMethod.POST)
+
+public String presentationprojet(Model model,  @ModelAttribute @Valid AmbientPower ambientPower, BindingResult bindingResult)
+{	AmbientPower ap = dao_ap.findByIdambientpower(32);
+
+model.addAttribute("idambientpower", ap);
+
+return "presentationambientpower";
+	
+}
+
+}
