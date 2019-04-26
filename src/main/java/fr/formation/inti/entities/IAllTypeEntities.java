389bb89ba48838f18a4561d1033d91723290f450
IN-ST-013 <IN-ST-013@IN-ST-013.home>
2019-04-26 16:51:16 +0200

    qtn - RestController pour obtenir all projects in json

---
+++ src/main/java/fr/formation/inti/entities/IAllTypeEntities.java
@@ -0,0 +1,11 @@
+package fr.formation.inti.entities;
+
+import java.util.Date;
+
+public interface IAllTypeEntities {
+	
+
+	public Date getDatecreation();
+	public void setDatecreation(Date datecreation);
+
+}
